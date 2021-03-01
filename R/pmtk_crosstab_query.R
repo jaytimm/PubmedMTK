02e981d0d9e7185ad15a07f7baf33b59e6c15d24
Jason Timm <jaetimm@gmail.com>
2021-03-01 09:14:02 -0700

    re

--- R/pmtk_crosstab_query.R
+++ R/pmtk_crosstab_query.R
@@ -26,11 +26,13 @@ pmtk_crosstab_query <- function(search_results){
   v2 <- reshape2::melt(v1, 'search', c(2:ncol(v1)))
   colnames(v2) <- c('term1', 'term2', 'n1n2')
   
+  v2
   ## 
   v3 <- subset(v2, term1 == term2)
   v4 <- merge(v2, data.frame(term1 = v3$term1, n1 = v3$n))
   v5 <- merge(v4, data.frame(term2 = v3$term1, n2 = v3$n))
-  v5$term2 <- as.character(term2)
+  
+  v5$term2 <- as.character(v5$term2)
   v5 <- v5[order(v5$term1),]
   v5 <- subset(v5, term1 != term2)
   v5[, c(2, 1, 4, 5, 3)]
