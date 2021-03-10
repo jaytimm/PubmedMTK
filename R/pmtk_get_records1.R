6f2f1daa5c8f438fafbb8d042dc9975ad1565685
Jason Timm <jaetimm@gmail.com>
2021-03-10 05:45:24 -0700

    re

--- R/pmtk_get_records1.R
+++ R/pmtk_get_records1.R
@@ -2,7 +2,7 @@
 #'
 #' @name pmtk_get_records1
 #' @param x A character vector of PMIDs
-#' @return A data frame.
+#' @return A data frame
 #' 
 #' 
 #' @export
@@ -14,6 +14,50 @@ pmtk_get_records1 <- function (x) {
                                         rettype = "xml", 
                                         parsed = T)
   
-  as(fetch.pubmed, "character")}
-  ########
+  pmtk_strip_xml1(as(fetch.pubmed, "character"))
+  }
   
+
+
+pmtk_strip_xml1 <- function (x) {
+  
+  newData <- XML::xmlParse(x)
+  records <- XML::getNodeSet(newData, "//PubmedArticle")
+  
+  pmid <- XML::xpathSApply(newData,"//MedlineCitation/PMID", 
+                           XML::xmlValue)
+  
+  year <- lapply(records, XML::xpathSApply, ".//PubDate/Year", 
+                 XML::xmlValue) 
+  year[sapply(year, is.list)] <- NA
+  year[which(sapply(year, is.na) == TRUE)] <- lapply(records[which(sapply(year, is.na) == TRUE)], 
+                                                     XML::xpathSApply, ".//PubDate/MedlineDate", XML::xmlValue)
+  
+  ## as date formal -- !!! -- 
+  year <- gsub(" .+", "", year)
+  year <- gsub("-.+", "", year)
+  
+  articletitle <- lapply(records, XML::xpathSApply, ".//ArticleTitle", XML::xmlValue) 
+  articletitle[sapply(articletitle, is.list)] <- NA
+  articletitle <- unlist(articletitle)
+  
+  meshHeadings <- lapply(records, XML::xpathSApply, ".//DescriptorName", XML::xmlValue)
+  meshHeadings[sapply(meshHeadings, is.list)] <- NA
+  meshHeadings <- sapply(meshHeadings, paste, collapse = "|")
+  
+  chemNames <- lapply(records, XML::xpathSApply, ".//NameOfSubstance", XML::xmlValue)
+  chemNames[sapply(chemNames, is.list)] <- NA
+  chemNames <- sapply(chemNames, paste, collapse = "|")
+  
+  keywords <- lapply(records, XML::xpathSApply, ".//Keyword", XML::xmlValue)
+  keywords[sapply(keywords, is.list)] <- NA
+  keywords <- sapply(keywords, paste, collapse = "|")
+  
+  y <- data.frame(pmid, year, articletitle,
+                  meshHeadings, chemNames, 
+                  keywords)
+  
+  #Sys.sleep(0.1)
+  Encoding(rownames(y)) <- 'UTF-8'    
+  return(y)
+}
